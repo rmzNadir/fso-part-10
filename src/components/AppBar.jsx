0f2c25b2943b2e6e7ddbca5ec3f9707cec720273
rmzNadir <dgzrz99@gmail.com>
2021-03-14 16:44:00 -0600

    Exercise 10.7

--- src/components/AppBar.jsx
+++ src/components/AppBar.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, ScrollView } from 'react-native';
 import Constants from 'expo-constants';
 import AppBarTab from './AppBarTab';
 import theme from '../theme';
@@ -18,8 +18,10 @@ const styles = StyleSheet.create({
 const AppBar = () => {
   return (
     <View style={styles.container}>
-      <AppBarTab title='Repositories' to='/' />
-      <AppBarTab title='SignIn' to='/signIn' />
+      <ScrollView horizontal>
+        <AppBarTab title='Repositories' to='/' />
+        <AppBarTab title='SignIn' to='/signIn' />
+      </ScrollView>
     </View>
   );
 };
