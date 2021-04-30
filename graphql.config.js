3aa9ae69dd9c0d6372af2304ab8896d5924a910b
rmzNadir <dgzrz99@gmail.com>
2021-04-29 20:56:49 -0500

    Exercise 10.12

--- graphql.config.js
+++
@@ -1,4 +0,0 @@
-// eslint-disable-next-line no-undef
-module.exports = {
-  schema: 'https://localhost:3001',
-};
--- src/utils/apolloClient.js
+++ src/utils/apolloClient.js
@@ -1,8 +1,9 @@
 import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+import Constants from 'expo-constants';
 
 const httpLink = createHttpLink({
   // Replace the IP address part with your own IP address!
-  uri: 'http://192.168.1.66:3001/graphql',
+  uri: Constants.manifest.extra.apolloUri,
 });
 
 const createApolloClient = () => {
